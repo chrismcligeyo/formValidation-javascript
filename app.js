169cea1c7322f65e21db57e182579a9b2898caae
= <=>
2019-11-05 14:48:25 +0300

    Validation complete.

---
+++ app.js
@@ -0,0 +1,49 @@
+// Form Blur Event Listeners
+document.getElementById('name').addEventListener('blur', validateName);
+document.getElementById('zip').addEventListener('blur', validateZip);
+document.getElementById('email').addEventListener('blur', validateEmail);
+document.getElementById('phone').addEventListener('blur', validatePhone);
+
+function validateName() {
+  const name = document.getElementById('name');
+  const re = /^[a-zA-Z]{2,10}$/; //sarts with either capital or lowrcase a to z with 2 to ten letters
+
+  if(!re.test(name.value)){
+    name.classList.add('is-invalid'); //is-invalid is a bootstrap class that that makes input invalid, you have to validate it yourself a
+  } else {
+    name.classList.remove('is-invalid');
+  }
+}
+
+function validateZip() {
+  const zip = document.getElementById('zip');
+  const re = /^[0-9]{5}(-[0-9]{4})?$/;//starts with 5 vnumbers between 0 to 9, with 4 numbers between 0 t0 9with a dash 00938-9324
+
+  if(!re.test(zip.value)){
+    zip.classList.add('is-invalid');
+  } else {
+    zip.classList.remove('is-invalid');
+  }
+}
+
+function validateEmail() {
+  const email = document.getElementById('email');
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/; //able to have letters, numbers, underscores, dashes and dots then @, then
+
+  if(!re.test(email.value)){
+    email.classList.add('is-invalid');
+  } else {
+    email.classList.remove('is-invalid');
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById('phone');
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if(!re.test(phone.value)){
+    phone.classList.add('is-invalid');
+  } else {
+    phone.classList.remove('is-invalid');
+  }
+}
